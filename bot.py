04959d3829a01d510421daae3eee1cf9a23fcc4c
Kira Midru <kirubelmidru@gmail.com>
2025-07-02 16:56:41 +0300

    minor fixes

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 
 from dotenv import load_dotenv
 from telethon.sync import TelegramClient
-from telethon.tl.functions.contacts import ImportContactsRequest
+from telethon.tl.functions.contacts import ImportContactsRequest, DeleteContactsRequest
 from telethon.tl.types import InputPhoneContact
 
 load_dotenv()
@@ -16,17 +16,17 @@ client = TelegramClient('verify_session', api_id, api_hash)
 client.start(phone=phone)
 
 hashmap = {}
-target = ['+251986558241']
+target = ['+251986558241', '+251911611891', '+251911120867', '+251961281789', '+251961281790', '+251966030844']
 
 for num in target:
-    contact = InputPhoneContact(client_id=0, phone=num, first_name='Test', last_name='User')
-    for input in phone:
-        result = client(ImportContactsRequest([contact]))
-        if result.users:
-            hashmap[num] = True
-        else:
-            hashmap[num] = False
-        time.sleep(1)
+    contact = InputPhoneContact(client_id=0, phone=num, first_name=num, last_name='User')
+    result = client(ImportContactsRequest([contact]))
+    if result.users:
+        hashmap[num] = True
+        client(DeleteContactsRequest(id=[num]))
+    else:
+        hashmap[num] = False
+    time.sleep(1)
 
 with open("output.json", "w") as file:
     json.dump(hashmap, file, indent=4)
